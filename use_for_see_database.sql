896d4a079a44bb098dc98147ce564dcf63c99e73
Fahim <fahimyadgir07@gmail.com>
2026-05-27 12:15:22 +0530

    change data type and find highest avg_cur_bal from finance_customer

--- use_for_see_database.sql
+++ use_for_see_database.sql
@@ -14,9 +14,8 @@ where Tot_cur_bal = (select max(Tot_cur_bal)from finance_customer);
 alter table finance_customer
 rename column emp_length to experienc;
 
-create view Full_details as
-select * from finance_customer
-where customer_id != 'none' and emp_title != 'none'  and experienc != 'none' and home_ownership != 'none' and annual_inc != 'none' and annual_inc_joint != 'none' and verification_status != 'none' and verification_status != 'none' and zip_code != 'none' and addr_state != 'none' and avg_cur_bal != 'none' and tot_cur_bal != 'none' ;
-drop view Full_details;
+alter table finance_customer
+modify avg_cur_bal bigint;
 
-select * from Full_details;
+select * from finance_customer
+where avg_cur_bal = (select max(avg_cur_bal)from finance_customer);
